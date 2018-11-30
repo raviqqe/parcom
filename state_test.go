6b2cf03e0702900dcc2dcb2b8115d7f98938b4ab
Yota Toyama <raviqqe@gmail.com>
2018-11-30 06:15:11 +0000

    Test state change

--- state_test.go
+++ state_test.go
@@ -15,6 +15,13 @@ func TestStateLineNumber(t *testing.T) {
 	assert.Equal(t, 1, parcom.NewState("").LineNumber())
 }
 
-func TestStateLinePosition(t *testing.T) {
+func TestStateCharacterPosition(t *testing.T) {
 	assert.Equal(t, 1, parcom.NewState("").CharacterPosition())
+}
+
+func TestStateWithNewLine(t *testing.T) {
+	s := parcom.NewState("\n")
+	s.Char('\n')()
+	assert.Equal(t, 2, s.LineNumber())
+	assert.Equal(t, 1, s.CharacterPosition())
 }
